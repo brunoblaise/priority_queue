298feab98f08b2a5680c2a803334680453268607
Bruno Blaise <brunoblaise94@gmail.com>
2022-08-18 14:41:13 +0200

    priority queue in js

---
+++ queue.js
@@ -0,0 +1,62 @@
+class Elements {
+  constructor(element, priority) {
+    this.element = element;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.collection = [];
+  }
+
+  enqueue(element, priority) {
+    const queue = new Elements(element, priority);
+
+    let contain = false;
+
+    for (let i = 0; i < this.collection.length; i++) {
+      if (this.collection[i].priority < queue.priority) {
+        this.collection.splice(i, 0, queue);
+        contain = true;
+
+        break;
+      }
+    }
+
+    if (!contain) {
+      this.collection.push(queue);
+    }
+  }
+
+  dequeue() {
+    return this.collection.shift();
+  }
+
+  peek() {
+    return this.collection[0];
+  }
+  rear() {
+    return this.collection[this.collection.length - 1];
+  }
+
+  get isEmpty() {
+    return this.collection.length === 0;
+  }
+
+  get print() {
+    return console.log(this.collection);
+  }
+}
+
+const pQ = new PriorityQueue();
+
+pQ.enqueue('john', 3);
+pQ.enqueue('mike', 2);
+pQ.enqueue('log', 1);
+
+pQ.dequeue();
+
+console.log(pQ.peek());
+console.log(pQ.rear());
+pQ.print;
